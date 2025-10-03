8ed1eeb4f43c69f8d70a283fc3d4578bef64582e
Chien <buikhacchien.work@gmail.com>
2025-10-03 08:59:06 +0700

    init

---
+++ airflow/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+export PATH="/home/airflow/.local/bin:$PATH"
+
+# Initialize database
+airflow db init
+
+# Create admin user if it doesn't exist
+airflow users create \
+    --username admin \
+    --password admin \
+    --firstname Admin \
+    --lastname User \
+    --role Admin \
+    --email admin@example.com
+
+exec "$@"
