82746d93112365439580363c9408989bed40e7f3
Chien <buikhacchien.work@gmail.com>
2025-11-17 11:40:03 +0700

    update-ui

---
+++ utils/helpers.py
@@ -0,0 +1,19 @@
+import logging
+from sqlalchemy import text
+import yaml
+
+# Logger
+def create_logger(name):
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
+    logger = logging.getLogger(name)
+    return logger
+
+def load_cfg(cfg_file):
+    cfg = None
+    with open(cfg_file, "r") as f:
+        try:
+            cfg = yaml.safe_load(f)
+        except yaml.YAMLError as e:
+            print(e)
+    return cfg
